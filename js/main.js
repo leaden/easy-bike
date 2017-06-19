83190865b9850044b633c455af132a5a5094c8a6
maribel <maribel.diaz.j@gmail.com>
2017-06-19 12:36:51 -0400

    mapa funcional

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function initMap(){
-	var map = new google.maps.Map(document.getElementById("map"){
+	var map = new google.maps.Map(document.getElementById("map"),{
 		zoom: 5,
 		center: {lat: -91191427, lng: -77.0349046},
 		mapTypeControl:false,
